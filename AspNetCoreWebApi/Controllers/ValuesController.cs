3a945f58f7eb5792327355aea2ac4c7fad21e4f8
Makwena <mahlomakwena@gmail.com>
2022-04-30 17:04:46 +0200

    "First commit"

---
+++ AspNetCoreWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestApi.EmployeeData;
+using RestApi.Models;
+
+namespace AspNetCoreWebApi.Controllers
+{
+    
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /* GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+        private IEmployeeData _employeeData;
+
+        public ValuesController(IEmployeeData employeeData)
+        {
+            _employeeData = employeeData;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_employeeData.GetEmployees());
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            _employeeData.AddEmployee(employee);
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + employee.Id, employee);
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetEmployee(Guid id)
+        {
+            var tempEmployee = _employeeData.GetEmployee(id);
+            if (tempEmployee != null)
+            {
+
+                return Ok(_employeeData.GetEmployee(id));
+            }
+            return NotFound($"Employee with id: {id} does not exist");
+        }
+
+    }
+}
---
+++ AspNetCoreWebApi/EmployeeData/MockEmployeeData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestApi.Models;
+
+namespace RestApi.EmployeeData
+{
+    public class MockEmployeeData : IEmployeeData
+    {
+        private List<Employee> employees=new List<Employee>()
+        {
+            new Employee()
+            {
+                Id=Guid.NewGuid(),
+                Name="EmployeeOne"
+            },
+            new Employee()
+            {
+                Id=Guid.NewGuid(),
+                Name="EmployeeTwo"
+            }
+        };
+        
+        public Employee AddEmployee(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            employees.Add(employee);
+            return employee;
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee EditEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
